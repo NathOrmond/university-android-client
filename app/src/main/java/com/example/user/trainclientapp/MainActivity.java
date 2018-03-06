5cc46cebfdb60d3d0db37fcee57b32f75428b771
NathOrmond <nath.m.ormond@gmail.com>
2018-03-06 14:33:46 +0000

    adapter

--- app/src/main/java/com/example/user/trainclientapp/MainActivity.java
+++ app/src/main/java/com/example/user/trainclientapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.trainclientapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{
+                    "Android.Permission.ACCESS_FINE_LOCATION",
+                    "Android.Permission.ACCESS_COARSE_LOCATION"
+            },1);
+
+        }
 
         Button findStationBtn = (Button) findViewById(R.id.stationButton);
     }
