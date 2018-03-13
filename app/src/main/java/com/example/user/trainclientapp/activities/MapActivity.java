07ed7063e23ba4f7590d06f088e6ef9eae75f922
NathOrmond <nath.m.ormond@gmail.com>
2018-03-13 19:20:21 +0000

    gradle changes

---
+++ app/src/main/java/com/example/user/trainclientapp/activities/MapActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.trainclientapp.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.user.trainclientapp.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
